ad270f098e239d692d1efba665d1b54b05efce99
Borgieee <mikielegroeg@gmail.com>
2025-08-06 02:48:28 +0800

    Update example_1.cpp

--- testing-main/nig/example_1.cpp
+++ testing-main/nig/example_1.cpp
@@ -1,6 +1,40 @@
 
 
+#include <iostream> //allows for basic input and output operation
+#include <conio.h> //enables reading input from the keyboard
 
+int main(){//our main function
+	int a = 5; //variables
+	int b = 2;
+	std::cout << "Welcome" << std::endl; //prints "Welcome" and moves to a new line
+	std::cout << "Greetings" << std::endl; //prints "Greetings" and moves to a new line
+	int c = a + b; //manipulating the variables to form another variable
+	std::cout << c; //displays the value of c 
+	getch(); //keeps the console open until it recieves a feedback from a key
+	return 0; //ends the program
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // operating with variables
 #include <iostream>
 #include <conio.h>
@@ -22,7 +56,7 @@ getch();
 return 0;
 
 }
-
+*/
 
 
 
